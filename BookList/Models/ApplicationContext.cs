d62c80a3a3f7af53a4195589dc1d8f89af3f8af1
dharmikavyasshukla <dharmikavyas173@gmail.com>
2021-03-10 11:15:29 +1000

    Add project files.

---
+++ BookList/Models/ApplicationContext.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+//using System.Data.Entity;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookList.Models
+{
+    public class ApplicationContext:DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-PL5J381;Database=Book;Trusted_Connection=True;");
+        }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Book>().ToTable("Book");
+            
+        }
+    }
+}
